ecedfb2f72eca9124858966d71edcf03e5fac62f
Manav Misra <manavm@visionwebsoft.com>
2020-09-25 16:22:47 -0500

    :bug: Set 'prop types' to accept array of nodes

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,6 @@ import React from "react";
 
 const Main = ({ children }) => <main>{children}</main>;
 
-Main.propTypes = { children: PropTypes.element.isRequired };
+Main.propTypes = { children: PropTypes.arrayOf(PropTypes.node).isRequired };
 
 export default Main;

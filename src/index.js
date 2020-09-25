70f021aaae4f6ce6a064962123b2261c6e350f19
Manav Misra <manavm@visionwebsoft.com>
2020-09-25 13:43:59 -0500

    :lipstick: Inject 💉 typography theme 🤓

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Typography from "typography";
+import funstonTheme from "typography-theme-funston";
+
 import App from "./App";
 
 import "./index.css";
+
+const typography = new Typography(funstonTheme);
+typography.injectStyles();
 
 ReactDOM.render(
   <React.StrictMode>

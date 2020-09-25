c67abbef1ae03932e626acf6038f448eef1f4b11
Manav Misra <manavm@visionwebsoft.com>
2020-09-25 13:37:00 -0500

    :speech_balloon: Update home page text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const App = () => <h1>Trivia Game!</h1>
+const App = () => (
+  <>
+    <h1>Welcome to the Trivia Challenge!</h1>
+    <p>
+      You will be presented with 10 True or False{" "}
+      <span role="img" aria-label="question">
+        ❓
+      </span>
+      s.
+    </p>
+    <p>
+      Can you score{" "}
+      <span role="img" aria-label="100">
+        💯
+      </span>
+      %?
+    </p>
+  </>
+);
 
 export default App;

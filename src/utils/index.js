a849f2b5a2ff17802126e8ac06d374dfe714fbd6
Manav Misra <manavm@visionwebsoft.com>
2020-09-27 10:36:36 -0500

    :bulb: Add JS Docs style comments

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
+/**
+ * 'Convert' HTML character codes into 'real font characters'
+ * @param {string} str - contains HTML character codes
+ * @returns {string}
+ */
 export const decodeCharCodes = (str) => {
   const span = document.createElement("span");
   span.innerHTML = str;

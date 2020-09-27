a32ea08697765bfcad6c5d819652ea9d2ee8aae0
Manav Misra <manavm@visionwebsoft.com>
2020-09-27 14:59:08 -0500

    :heavy_plus_sign: 'Upgrade' to 'ky'

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
+import ky from "ky";
+
 export default (
   route = " https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean"
 ) => ({
   async index() {
-    const res = await fetch(route);
+    const res = await ky.get(route);
     return res;
   },
 });

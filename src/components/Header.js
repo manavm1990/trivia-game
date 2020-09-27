cadcfb86374da402f725c1596b63e158f7052857
Manav Misra <manavm@visionwebsoft.com>
2020-09-27 10:47:54 -0500

    :fire: Simplify 'Header'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,9 @@
-import PropTypes from "prop-types";
 import React from "react";
 
-import { Link } from "react-router-dom";
-
-const Header = ({ txt }) => (
-  <Link to="/">
-    <header>
-      <h1>{txt}</h1>
-    </header>
-  </Link>
+const Header = () => (
+  <header>
+    <h1>Trivia Challenge!</h1>
+  </header>
 );
-
-Header.propTypes = { txt: PropTypes.string };
-
-Header.defaultProps = { txt: "Welcome to the Trivia Challenge!" };
 
 export default Header;
